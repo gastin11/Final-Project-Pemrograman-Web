0493eeae37fc3f631636657a3e1b3f4e52f9743a
gastin11 <gastinalfiansa@gmail.com>
2024-05-02 10:44:55 +0700

    update progress

--- dashboard.php
+++ dashboard.php
@@ -58,7 +58,7 @@ $level_admin = $_SESSION['level_admin'];
                     <div class="sb-sidenav-menu">
                         <div class="nav">
                             <div class="sb-sidenav-menu-heading">Menu</div>
-                            <a class="nav-link" href="#">
+                            <a class="nav-link" href="dashboard.php">
                                 <div class="sb-nav-link-icon"><i class="fas fa-tachometer-alt"></i></div>
                                 Dashboard
                             </a>
