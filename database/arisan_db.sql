bf0366b5167a6eafffc4d8a0e319342502c98e26
gastin11 <gastinalfiansa@gmail.com>
2024-05-08 21:38:08 +0700

    update progress

--- database/arisan_db.sql
+++ database/arisan_db.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Apr 28, 2024 at 06:41 PM
+-- Generation Time: May 08, 2024 at 04:36 PM
 -- Server version: 10.4.32-MariaDB
 -- PHP Version: 8.2.12
 
@@ -43,6 +43,69 @@ INSERT INTO `admin` (`id_admin`, `email_admin`, `nama_admin`, `password`, `level
 (1, 'ketua@gmail.com', 'ketua', '00719910bb805741e4b7f28527ecb3ad', 'ketua'),
 (2, 'sekretaris@gmail.com', 'sekretaris', 'ce1023b227de5c34b98c470cda4699bb', 'sekretaris'),
 (3, 'bendahara@gmail.com', 'bendahara', 'c9ccd7f3c1145515a9d3f7415d5bcbea', 'bendahara');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tb_anggota`
+--
+
+CREATE TABLE `tb_anggota` (
+  `id_anggota` varchar(255) NOT NULL,
+  `nama` varchar(30) NOT NULL,
+  `noTelpon` varchar(15) NOT NULL,
+  `email` varchar(30) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `tb_anggota`
+--
+
+INSERT INTO `tb_anggota` (`id_anggota`, `nama`, `noTelpon`, `email`) VALUES
+('AGT001', 'nolan', '099988880000', 'nolanez@gmail.com'),
+('AGT002', 'gastin alfiansa', '7777777777777', 'gastin@gmail.com');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tb_pertemuan`
+--
+
+CREATE TABLE `tb_pertemuan` (
+  `id_pertemuan` int(12) NOT NULL,
+  `lokasi` varchar(40) NOT NULL,
+  `tanggal` date NOT NULL,
+  `jam` varchar(15) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `tb_pertemuan`
+--
+
+INSERT INTO `tb_pertemuan` (`id_pertemuan`, `lokasi`, `tanggal`, `jam`) VALUES
+(240508001, 'rumah abcdef', '2024-05-09', 'jam 7 malam');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `admin`
+--
+ALTER TABLE `admin`
+  ADD PRIMARY KEY (`id_admin`);
+
+--
+-- Indexes for table `tb_anggota`
+--
+ALTER TABLE `tb_anggota`
+  ADD PRIMARY KEY (`id_anggota`);
+
+--
+-- Indexes for table `tb_pertemuan`
+--
+ALTER TABLE `tb_pertemuan`
+  ADD PRIMARY KEY (`id_pertemuan`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
